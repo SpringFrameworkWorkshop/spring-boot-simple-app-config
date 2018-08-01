15ef96acd52cc01cf75ef155fa3343de0628e2f9
Pablo Benito González <pablobenitogonzalez@gmail.com>
2018-08-01 19:45:21 +0200

    added actuator

--- src/main/java/io/spring/springbootsimpleapp/HomeController.java
+++ src/main/java/io/spring/springbootsimpleapp/HomeController.java
@@ -1,10 +1,13 @@
 package io.spring.springbootsimpleapp;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.Setter;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @ConfigurationProperties(prefix="taco.orders")
@@ -14,9 +17,16 @@ public class HomeController {
 
     private int limit = 200;
 
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     @GetMapping("/")
-    public String home() {
+    public String home(@RequestParam(value ="name", required= false) final String name) {
         log.info(String.format(">>>>>>>>>>>>>>> Order limit: [%s]", limit));
+        if (name != null) {
+            log.info(String.format(">>>>>>>>>>>>>>> Retrieved param name: [%s]", name));
+            meterRegistry.counter("taco."+name.toLowerCase()).increment();
+        }
         return "home";
     }
 
